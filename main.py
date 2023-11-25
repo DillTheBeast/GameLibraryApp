cb143aefe5b1bcdb3f31b15ffdedd9d5e6b7465a
DillTheBeast <dillonmaltese@gmail.com>
2023-11-25 15:18:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import PySimpleGUI as sg
 
 WIDTH, HEIGHT = 800, 800
 custom_font = ('Helvetica', 20)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (200, 200, 200)
+GREY = (169, 169, 169)
+
+def rgbToHex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(*rgb)
+
+BLACK = rgbToHex(BLACK)
+BLUE = rgbToHex(BLUE)
+WHITE = rgbToHex(WHITE)
+GREY = rgbToHex(GREY)
 
 def main():
     # Define the layout of the window
     layout = [
-        [sg.Button('Click me', font = custom_font, size = (20, 10))],
+        [sg.Button('Add a Game', font = custom_font, size = (80, 2), button_color = (WHITE, GREY))],
     ]
 
     # Create the window with a specific size
-    window = sg.Window('Bigger PySimpleGUI App', layout, size = (WIDTH, HEIGHT))
+    window = sg.Window('Game Library', layout, size = (WIDTH, HEIGHT), background_color = BLACK)
 
     # Event loop
     while True:
         event, values = window.read()
 
-        # If the user closes the window or clicks the button, exit the loop
-        if event == sg.WINDOW_CLOSED or event == 'Click me':
+        if event == sg.WINDOW_CLOSED or event == 'Add a Game':
             break
 
     # Close the window
